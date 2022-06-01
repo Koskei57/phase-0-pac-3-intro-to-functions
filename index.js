0705927bd2c16f397cd27647c9ee69e99a401760
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-01 18:16:10 +0300

    commit tree 1383e41334ac890991c65781ae9beb2d5b1465f7

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Follow along with the examples here
+/*function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+}
+sayHello();
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+}*/
+
+//order of arguments
+function say(firstName, greeting) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+say("Julio", "hello");
+
+function say(greeting, firstName) {
+
+    console.log("I was called!");
+    console.log(say("Howdy", "partner"));
+    return `${greeting}, ${firstName}!`;
+}
